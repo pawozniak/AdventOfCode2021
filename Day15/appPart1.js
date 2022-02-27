eff27915a6a02b7f40f1ef203cd02eba3f33c6e0
pawozniak <35077210+pawozniak@users.noreply.github.com>
2022-02-27 12:42:57 +0100

    Update appPart1.js

--- Day15/appPart1.js
+++ Day15/appPart1.js
@@ -2,7 +2,7 @@ $(function(){
     $.ajax({
         url: './inputDay15.csv'
     }).done(function(response){
-        
+
         // Prepare data
         let data = $.csv.toArrays(response);
         console.log(data);
@@ -19,7 +19,7 @@ $(function(){
         object.previousVertex = {};
         object.unvisited = {};
 
-        // Assign distance to nodes (0 to start nodes, infinity to all other nodes)
+        // Assign distance to nodes (0 to start node, infinity to all other nodes)
         for(let i=0; i<dimension; i++){
             for(let j=0; j<dimension; j++){
                 object.unvisited[[i,j]]='';
@@ -39,33 +39,36 @@ $(function(){
 
         // Repeat while there are unvisited nodes left
         while(Object.keys(object.unvisited).length>0){
-        // Visit unvisited vertex with smalles distance from the start
-        let currentVertex = Object.keys(object.unvisited).reduce((key, v) => object.unvisited[v] < object.unvisited[key] ? v : key);
-        currentVertex = currentVertex.split(',').map(Number);
+            // Visit unvisited vertex with smalles distance from the start
+            let currentVertex = Object.keys(object.unvisited).reduce((key, v) => object.unvisited[v] < object.unvisited[key] ? v : key);
+            currentVertex = currentVertex.split(',').map(Number);
 
-        // For current vertex examine unvisited neighbours
-        let currentVertexNeighbours = [];
-        for(let i=-1; i<2; i++){
-            for(let j=-1; j<2; j++){
-                if(data[currentVertex[0]+i] !== undefined && data[currentVertex[0]+i][currentVertex[1]+j] !== undefined && Math.abs(i)!==Math.abs(j)){
-                    currentVertexNeighbours.push([currentVertex[0]+i,currentVertex[1]+j]);
+            // For current vertex examine unvisited neighbours
+            let currentVertexNeighbours = [];
+            for(let i=-1; i<2; i++){
+                for(let j=-1; j<2; j++){
+                    if(data[currentVertex[0]+i] !== undefined && data[currentVertex[0]+i][currentVertex[1]+j] !== undefined && Math.abs(i)!==Math.abs(j)){
+                        currentVertexNeighbours.push([currentVertex[0]+i,currentVertex[1]+j]);
+                    }
                 }
             }
-        }
 
-        // For the current vertex calculate the distance of each neighbour from the start
-        currentVertexNeighbours.forEach(element => {
-            let calculatedDistance = object.shortestDistance[currentVertex]+data[element[0]][element[1]];
-            
-            if(calculatedDistance<object.unvisited[element]){
-                object.unvisited[element] = calculatedDistance;
-                object.shortestDistance[element] = calculatedDistance;
-                object.previousVertex[element] = currentVertex;
+            // For the current vertex calculate the distance of each neighbour from the start
+            currentVertexNeighbours.forEach(element => {
+                let calculatedDistance = object.shortestDistance[currentVertex]+data[element[0]][element[1]];
+                
+                if(calculatedDistance<object.unvisited[element]){
+                    object.unvisited[element] = calculatedDistance;
+                    object.shortestDistance[element] = calculatedDistance;
+                    object.previousVertex[element] = currentVertex;
+                }
+            });
+
+            // Remove current vertex from the list of unvisited nodes
+            delete object.unvisited[currentVertex];
+            if(currentVertex == [dimension-1,dimension-1]){
+                break;
             }
-        });
-
-        // Remove current vertex from the list of unvisited nodes
-        delete object.unvisited[currentVertex];
         }
 
         console.log(object.shortestDistance[[dimension-1,dimension-1]])
